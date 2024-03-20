814019617ae7b75fbdaca8a4b1f205270411ce30
soun Soratha <121934976+ZensYing@users.noreply.github.com>
2024-03-20 12:43:43 +0700

    update function catch error

--- plugins/supabaseClient.js
+++ plugins/supabaseClient.js
@@ -5,5 +5,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://axzqeugbjsdwpetkilrv.supabase.co'
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
-
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Supabase URL and Anon Key are required.')
+  }
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
